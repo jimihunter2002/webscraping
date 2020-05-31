419cba473aeb911bec527d486b698a9ad4c0d82e
jimihunter2002 <jimi.hunter008@gmail.com>
2020-05-31 19:55:15 +0100

    added test for null value

--- src/test/e2e/arrayCenter.test.js
+++ src/test/e2e/arrayCenter.test.js
@@ -12,7 +12,7 @@ describe('array center', () => {
       arraySumFromLeft([10, 15, 5, 7, 1, 24, 36, 2]),
       [10, 15, 5, 7, 1, 24, 36, 2],
     );
-    expect(result).toBe(4);
+    expect(result).toBe(5);
   });
 
   test('returned index 5 for array [ 23, 50, 63, 90, 10, 30, 155, 23, 18 ]', () => {
@@ -22,5 +22,14 @@ describe('array center', () => {
       [23, 50, 63, 90, 10, 30, 155, 23, 18],
     );
     expect(result).toBe(4);
+  });
+
+  test('returned index null for array [30,	43,	29,	10,	50,	40,	99,	51,	12,2, 7]', () => {
+    const result = arrayCenterIndex(
+      arraySumFromRight([30, 43, 29, 10, 50, 40, 99, 51, 12, 2, 7]),
+      arraySumFromLeft([30, 43, 29, 10, 50, 40, 99, 51, 12, 2, 7]),
+      [30, 43, 29, 10, 50, 40, 99, 51, 12, 2, 7],
+    );
+    expect(result).toBeNull();
   });
 });
